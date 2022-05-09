a938a633a705a93566851540a6bd50bed68d72b8
Narayan, Deep <Deep.Narayan@BakerHill.com>
2022-05-09 18:12:54 +0530

    commit changes

---
+++ ReceiveMessageServiceBus2/ReceiveMessage.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveMessageServiceBus2
+{
+    public class ReceiveMessage
+    {
+        static string connectionString = "Endpoint=sb://notificationservice0.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=b4ojYUDXIBKvtL6s1IN4JsNHUvG/KGVzIv33ta0nBHg=";
+
+        static string topicName = "notification0";
+
+
+
+        public async Task ReceiveMessageFromTopic()
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusReceiver receiver = client.CreateReceiver(topicName,"subscriber2", new ServiceBusReceiverOptions { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+            string body = receivedMessage.Body.ToString();
+            Console.WriteLine(body);
+
+
+
+            Console.WriteLine("Received Messsage from topic subscription 2.");
+            Console.ReadKey();
+        }
+
+    }
+}
